676d774dd098dd59272990361f5eb7ad0bbe53f4
zerocool-11 <yash.kumar@optisolbusiness.com>
2022-03-31 00:42:37 +0530

    added requirements

--- main-espcam.py
+++ main-espcam.py
@@ -1,30 +1,25 @@
 from turtle import position
 from utils.FaceDetector import mp_detector
-import mediapipe as mp
 import numpy as np
 import cv2
 import time
 from websocket import create_connection
-import PIL.Image as Image
 from time import sleep
 import threading 
 import io
 import urllib
 
+ip='192.168.1.67'
 # ws = create_connection('ws://192.168.1.59/Camera')
-ws1=create_connection('ws://192.168.1.67/ServoInput')
-class w:
-    def __init__(self) -> None:
-        pass
-    def send(self,data):
-        pass
+ws1=create_connection('ws://{}/ServoInput'.format(ip))
+url="http://{}/picture".format(ip)
 
-# ws1=w()
-url="http://192.168.1.67/picture"
 detector=mp_detector()
+
 cap=cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
+
 pTime=0
 mid_x=None
 position=90
@@ -47,26 +42,6 @@ def blink():
         ws1.send(data.join(temp))
         
 
-def track(bbox,w,h,pid,pErrorW,pErrorH):
-    cx,cy=bbox[0],bbox[1]
-    errorW=cx-w//2
-    errorH=cy-h//2
-    speedW=pid[0]*errorW + pid[1]*(errorW-pErrorW)
-    speedW=int(np.clip(speedW,-90,90))
-    speedH=pid[0]*errorH + pid[1]*(errorH-pErrorH)
-    speedH=int(np.clip(speedH,-73,73))
-    if speedH >= 0:
-        speedH=speedH+73
-    else:
-        speedH=speedH+73
-    if speedW >= 0:
-        speedW=speedW+90
-    else:
-        speedW=(speedW)+90
-    print("sending speed y ",speedH)
-
-    return errorW,errorH,speedW,speedH
-
 old_speed_x=90
 old_speed_y=90
 
@@ -77,14 +52,6 @@ while True:
     imgResp=urllib.request.urlopen(url)
     imgNp=np.array(bytearray(imgResp.read()),dtype=np.uint8)
     img=cv2.imdecode(imgNp,-1)
-    # --------------uncomment till here----------
-    # x= ws.recv()
-    # image = Image.open(io.BytesIO(x))
-    # # print("done ",type(image))
-    # # image.save("x.jpg")
-    # img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    # success, img = cap.read()
-    # img=cv2.flip(img, 1)
     img=cv2.resize(img,(640,480))
     
     img, label,landmarks,rect,hand_id = detector.hand(img)
@@ -134,58 +101,12 @@ while True:
         cv2.putText(img, f'Gesture: {hand_id}', (20, 130), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
         t=threading.Thread(target=blink,args=())
         t.start()
-        #         
-        # pErrorW,pErrorH,position,position_y= track([mid_x,mid_y],cols,rows,pid,pErrorW,pErrorH)
-        # if old_speed_x>position:
-        #     if old_speed_x-position>5:  ##check if angle change more than 5 degree then only move
-        #         temp=["Tilt",str(position)]
-        #         data=","
-        #         ws1.send(data.join(temp))
-        #         print("data: ",data.join(temp))
-        #         cv2.putText(img, f'sending for x {position}', (20, 200), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-        #         old_speed_x=position
-        # elif old_speed_x<position:
-        #     if position-old_speed_x>5:  ##check if angle change more than 5 degree then only move
-        #         temp=["Tilt",str(position)]
-        #         data=","
-        #         print("data: ",data.join(temp))
-        #         ws1.send(data.join(temp))
-        #         cv2.putText(img, f'sending for x {position}', (20, 200), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-        #         old_speed_x=position
-        # if old_speed_y>position_y:
-        #     if old_speed_y-position_y > 5:  ##check if angle change more than 5 degree then only move            
-        #         if position_y<73:
-        #             temp_y=["Pan",str(73)]
-        #         else:
-        #             temp_y=["Pan",str(position_y)]
-        #         data_y=","
-        #         ws1.send(data_y.join(temp_y))
-        #         cv2.putText(img, f'sending for y {position}', (20, 230), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-        #         print("data: ",data_y.join(temp_y))
-        #         old_speed_y=position_y
-        # elif old_speed_y<position_y:
-        #     if position_y-old_speed_y >5:  
-        #         if position_y<73:
-        #             temp_y=["Pan",str(73)]
-        #         else:
-        #             temp_y=["Pan",str(position_y)]
-                
-        #         data_y=","
-        #         ws1.send(data_y.join(temp_y))
-        #         cv2.putText(img, f'sending for y {position}', (20, 230), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-        #         print("data: ",data_y.join(temp_y))
-        #         old_speed_y=position_y
-                
-    
-    
-    
+        
     
     
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    # cv2.putText(img, f'Position x: {position}', (20, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-    # cv2.putText(img, f'Position y: {position_y}', (20, 150), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
     
     cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
     cv2.imshow("Image", img)
